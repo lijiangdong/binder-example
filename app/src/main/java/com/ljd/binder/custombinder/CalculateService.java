4c21f95ccda801938d3a422eab1ac462cc7b6762
ljd2038 <ljd2038@gmail.com>
2016-02-24 14:23:44 +0800

    custom binder

--- app/src/main/java/com/ljd/binder/custombinder/CalculateService.java
+++ app/src/main/java/com/ljd/binder/custombinder/CalculateService.java
@@ -2,10 +2,8 @@ package com.ljd.binder.custombinder;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.IBinder;
-import android.os.Parcel;
-import android.os.RemoteException;
+
 
 public class CalculateService extends Service {
     private Calculate mCalculate;
